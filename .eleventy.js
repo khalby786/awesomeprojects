a4a174f5b337a1ff58ee518ed4734bb11ad584ba
Glitch (hello-express) <none>
2020-11-22 21:58:51 +0000

    🎗💅 Checkpoint ./projects.html:8045597/268 ./index.html:8045597/19 ./package.json:8045597/1495 ./.eleventy.js:8045597/555

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
+var md = require("markdown-it")();
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addFilter("markdown", function(value) {
+    return md.render(value);
+  });
 };
